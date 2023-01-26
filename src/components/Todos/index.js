6c994e329048939f76d234e767efc7a480f85706
birsenbozkurt <birsenbozkurt4041@gmail.com>
2023-01-26 22:18:05 +0300

    todo-input

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { FaPencilAlt } from "react-icons/fa";
 function Todos() {
+	const [list, setList] = useState([]);
+	const [input, setInput] = useState("");
+
+	const addTodo = (todo) => {
+		const newTodo = {
+			id: Math.random(),
+			todo: todo,
+		};
+
+		setList([...list, newTodo]);
+		setInput("");
+	};
 	return (
 		<div>
 			<h1 className="title">Yapılacaklar Listesi</h1>
 			<form>
 				<div className="search-btn">
-					<input type="text" placeholder="İdeal düzenini inşa et..." className="inputText" />
-					<button>
+					<input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="İdeal düzenini inşa et..." className="inputText" />
+					<button onClick={() => addTodo(input)}>
 						<FaPencilAlt />
 					</button>
+					<ul>
+						{list.map((todo) => (
+							<li key={todo.id}>
+								{todo.todo}
+								<button>&times;</button>
+							</li>
+						))}
+					</ul>
 				</div>
 			</form>
 		</div>
